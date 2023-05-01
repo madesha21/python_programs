148a1220cd8b98fe68481af14eacc7b77ef4b141
madesha21 <madesharamegowda11@gmail.com>
2023-05-01 16:52:38 +0530

    This is simple calculator program

---
+++ simple_cal.py
@@ -0,0 +1,16 @@
+a=int(input('Enter the number a:  '))
+b=int(input('Enter the number b: ' ))
+
+add=a+b
+diff=a-b
+prod=a*b
+div=a/b
+rem=a%b
+
+print('The addition of a and b ',add)
+print('The difference between a and b : ',diff)
+print('The product of two numbers a and b : ',prod)
+print('The divide of two numbers : ',div)
+print('The remainder is : ',rem)
+
+
