121490a492a8e675e3c81ad175470fef4ad65838
madesha21 <madesharamegowda11@gmail.com>
2023-05-26 10:46:20 +0530

    Simple average calculation

---
+++ Average.py
@@ -0,0 +1,9 @@
+marksMaths=int(input('Enter maths marks'))
+marksEnglish=int(input('Enter English marks'))
+marksIP=int(input('Enter IP marks '))
+
+sum1=(marksMaths+marksEnglish+marksIP)
+Average=sum1/3
+
+print('Average is: ',Average)
+
